a4ccdcbbdf22b77e585e71547ee3a5649a00e958
cunbex <hamzalagab.tech@gmail.com>
2023-02-26 17:20:43 +0100

    Add Alpha version of code
    fixed some javascript problems.

--- script/script.js
+++ script/script.js
@@ -7,26 +7,27 @@ const rainbowMode = document.querySelector("#rainbowmode");
 const colorInput = document.querySelector("input[type=color]");
 const buttons = document.querySelectorAll("button");
 let colorType = 0;
+let runningColor = "black";
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+container.onmousedown = () => (mouseDown = true);
+container.onmouseup = () => (mouseDown = false);
 
 window.addEventListener("load", createDivs(16));
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     buttons.forEach((button) => {
       button.style.backgroundColor = "#2ecc71";
-      document.getElementById(e.target.id).style.backgroundColor = "#8aaa8a";
     });
+    document.getElementById(e.target.id).style.backgroundColor = "#8aaa8a";
   });
 });
-colorInput.addEventListener("change", callChangeColor);
+colorInput.addEventListener("change", (e) => {
+  runningColor = e.target.value;
+});
 colorMode.addEventListener("click", () => {
   colorType = 0;
-  colorInput.addEventListener("change", callChangeColor);
 });
-rainbowMode.addEventListener("click", (e) => {
-  colorInput.removeEventListener("change", callChangeColor);
+rainbowMode.addEventListener("click", () => {
   colorType = 1;
 });
 eraser.addEventListener("click", () => {
@@ -39,20 +40,11 @@ layout.addEventListener("change", (e) => {
 });
 
 clear.addEventListener("click", () => {
-  if (container.children.length === 0) {
-    confirm("Please pick a layout first");
-  }
   const pixelDivs = document.querySelectorAll(".draw");
   pixelDivs.forEach((pixelDiv) => {
     pixelDiv.style.backgroundColor = "white";
   });
 });
-function callChangeColor(e) {
-  changeRunningColor(e.target.value);
-}
-function changeRunningColor(color) {
-  runningColor = color;
-}
 function createDivs(numberOfDivs) {
   container.style.gridTemplateColumns = `repeat(${numberOfDivs}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${numberOfDivs}, 1fr)`;
